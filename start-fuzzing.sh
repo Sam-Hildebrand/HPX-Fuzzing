842969b5aaae8e23b340222623e89bbabcfd6578
Sam Hildebrand <sch_numberone@yahoo.com>
2023-11-28 14:22:30 -0600

    Fix to run script

--- start-fuzzing.sh
+++ start-fuzzing.sh
@@ -7,4 +7,5 @@
 # just rename it.
 singularity build --force HPX-Fuzzer.sif HPX-Fuzzer.def >> Fuzzing-Setup.log
 singularity instance run --writable-tmpfs HPX-Fuzzer.sif Fuzzer1
-#singularity instance stop Fuzzer1
+singularity shell instance://Fuzzer1
+singularity instance stop Fuzzer1

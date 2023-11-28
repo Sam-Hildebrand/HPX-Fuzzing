ae676b6e7b23f78502b1c04abb36c3c3abf4e963
Sam Hildebrand <sch_numberone@yahoo.com>
2023-11-28 12:58:54 -0600

    Created simple fuzzing example for ForEach, and automated running

--- start-fuzzing.sh
+++ start-fuzzing.sh
@@ -7,5 +7,4 @@
 # just rename it.
 singularity build --force HPX-Fuzzer.sif HPX-Fuzzer.def >> Fuzzing-Setup.log
 singularity instance run --writable-tmpfs HPX-Fuzzer.sif Fuzzer1
-singularity shell instance://Fuzzer1
-singularity instance stop Fuzzer1
+#singularity instance stop Fuzzer1

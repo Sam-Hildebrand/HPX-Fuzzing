61536e1137b20b334e2d6610ca16db0d365d80aa
Sam Hildebrand <sch_numberone@yahoo.com>
2023-11-28 16:56:30 -0600

    fixed script automation

--- HPX-Fuzz.sh
+++ HPX-Fuzz.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-/AFLplusplus/afl-fuzz -i /Fuzzing/HPX-Fuzzing/inputs -o Fuzzing-Output -- /Fuzzing/HPX-Fuzzing/build/foreach_fuzzer
+# This file contains the command to be run in order to start fuzzing HPX
+
+/AFLplusplus/afl-fuzz -i /Fuzzing/HPX-Fuzzing/inputs -o Fuzzing-Output -- /Fuzzing/HPX-Fuzzing/build/foreach_fuzzer >> HPX-Fuzzing.log
